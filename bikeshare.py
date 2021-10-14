e5b71129618d1905ebd217d93f2f35778bb9be8c
Nicole Heisler <nicole.heisler@gmail.com>
2021-10-14 15:29:27 -0400

    Updated print statements in bikeshare for consistency

--- bikeshare.py
+++ bikeshare.py
@@ -17,32 +17,32 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    
+
     city = input("Enter which city you'd like info for: {}:\n".format(list(CITY_DATA.keys()))).lower()
     while city not in list(CITY_DATA.keys()):
         city = input("Invalid city - please try again:\n")
         print("city entered:", city)
-    print("OK! Gathering information for {}...".format(city.title()))
-    
+    print("Gathering information for {}...".format(city.title()))
+
     month_names = ["all"]
     for i in range(1,7):
         month_names.append(calendar.month_name[i].lower())
-        
+
     month = input("Please enter which month you would like info for, or enter all:\n").lower()
     while month not in month_names:
         month = input("Invalid month - please try again:\n")
         print("month entered:", month)
-    print("Got it! We will provide information for {}...".format(month.title()))
+    print("Gathering information for {}...".format(month.title()))
 
     day_names = ["all", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-    
+
     day = input("Please enter a day of the week to get info for, or enter all:\n").lower()
     while day not in day_names:
         day = input("Invalid day - please try again:\n")
         print("day entered:", day)
-    print("OK, will show data for {}...".format(day.title()))
-    
-   
+    print("Gathering information for {}...".format(day.title()))
+
+
     print('-'*40)
     return city, month, day
 
@@ -67,7 +67,7 @@ def load_data(city, month, day):
     # extracts month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    
+
     # filters by month if applicable
     if month != 'all':
         # use the index of the months list to get the corresponding int
@@ -83,7 +83,7 @@ def load_data(city, month, day):
         df = df[df['day_of_week'] == day.title()]
     return df
 
-            
+
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
@@ -142,7 +142,7 @@ def trip_duration_stats(df):
     leftover_minutes = total_travel_time % 1440
     hours = leftover_minutes // 60
     mins = total_travel_time - (days*1440) - (hours*60)
-    
+
     print('Total travel time:', days, 'days', hours, 'hours', mins, 'minutes')
 
     # displays mean travel time
@@ -151,7 +151,7 @@ def trip_duration_stats(df):
     leftover_minutes = mean_travel_time % 1440
     hours = leftover_minutes // 60
     mins = mean_travel_time - (days*1440) - (hours*60)
-    
+
     print('Mean travel time:', days, 'days', hours, 'hours', mins, 'minutes')
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -163,16 +163,16 @@ def user_stats(df):
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
-    
+
     # displays counts of user types
     user_types = df['User Type'].value_counts()
     print('Counts of user types:', user_types)
-        
+
     try:
         # displays counts of gender
         gender_counts = df['Gender'].value_counts()
         print('Counts of gender:', gender_counts)
-        
+
         # displays earliest, most recent, and most common year of birth
         earliest_birth_year = df['Birth Year'].min()
         most_recent_birth_year = df['Birth Year'].max()
@@ -187,9 +187,9 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-    
+
 def raw_data(df):
-    """Asks user if they would like to see the raw data and displays 
+    """Asks user if they would like to see the raw data and displays
         5 rows of data at a time. """
     pd.set_option('display.max_columns', 200)
     start = 0
@@ -199,15 +199,15 @@ def raw_data(df):
         print(df_slice)
         start += 5
         raw_data = input('\nWould you like to see moreeeee data?! Enter yes or no.\n')
-        
-        
+
+
 def main():
     while True:
         city, month, day = get_filters()
         #city, month, day = ('chicago','january','monday')
         df = load_data(city, month, day)
-        
-       
+
+
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
